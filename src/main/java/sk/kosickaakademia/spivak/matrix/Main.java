578cc219b7ec71e6d8a415f24c7769fe7fb9f643
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-15 18:45:20 +0200

    Test 6x6 Matrix

--- src/main/java/sk/kosickaakademia/spivak/matrix/Main.java
+++ src/main/java/sk/kosickaakademia/spivak/matrix/Main.java
@@ -11,9 +11,10 @@ public class Main
         int[][] matrix3 = new int[][]{ {2,5,9},{8,9,-5},{1,8,7} };
         int[][] matrix4 = new int[][]{ {2,5,9,8},{8,9,-5,2},{1,8,7,9},{-2,4,0,7} };
         int[][] matrix5 = new int[][]{ {2,5,9,8,1},{8,9,-5,2,7},{1,8,7,9,2},{-2,4,0,7,2},{5,-7,3,1,6} };
+        int[][] matrix6 = new int[][]{ {2,5,9,8,1,6},{8,9,-5,2,7,4},{1,8,7,9,2,2},{-2,4,0,7,2,2},{5,-7,3,1,6,1},{4,2,-3,4,6,0} };
 
-        matrix.printMatrix(matrix5);
-        System.out.println(matrix.determinantNxNMatrix(matrix5));
+        matrix.printMatrix(matrix6);
+        System.out.println(matrix.determinantNxNMatrix(matrix6));//-46864
 
 
 
