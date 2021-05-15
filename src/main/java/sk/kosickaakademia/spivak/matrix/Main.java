8145801a9bc2c6d01c05d8b0dabadc2c1c93b9a7
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-15 13:51:52 +0200

    Determinant of 3x3 Matrix

--- src/main/java/sk/kosickaakademia/spivak/matrix/Main.java
+++ src/main/java/sk/kosickaakademia/spivak/matrix/Main.java
@@ -8,9 +8,12 @@ public class Main
         Matrix matrix = new Matrix();
         int[][] matrix1 = new int[][]{ {2,5,9,8,1},{8,9,-5,2,7},{1,8,7,9,2},{-2,4,0,7,2} };
         int[][] matrix2 = new int[][]{ {4,-5},{3,-2} };
+        int[][] matrix3 = new int[][]{ {2,5,9},{8,9,-5},{1,8,7} };
 
-        matrix.printMatrix(matrix2);
-        System.out.println(matrix.determinant2x2Matrix(matrix2));
+        matrix.printMatrix(matrix3);
+        System.out.println(matrix3[0][2]);
+        System.out.println(matrix.determinant3x3Matrix(matrix3));
+
 
     }
 
--- src/main/java/sk/kosickaakademia/spivak/matrix/Matrix.java
+++ src/main/java/sk/kosickaakademia/spivak/matrix/Matrix.java
@@ -62,4 +62,18 @@ public class Matrix {
         }
         return 0;
     }
+
+    /**
+     * Determinant of 3x3 Matrix
+     * @param matrix
+     * @return int
+     */
+    public int determinant3x3Matrix (int matrix[][]){
+        if(matrix.length == 3 && matrix[0].length == 3){
+            return mainDiagonal(matrix) + matrix[2][0]*matrix[0][1]*matrix[1][2]
+            + matrix[1][0]*matrix[2][1]*matrix[0][2] - secondaryDiagonal(matrix)
+            - matrix[0][0]*matrix[2][1]*matrix[1][2] - matrix[1][0]*matrix[0][1]*matrix[2][2];
+        }
+        return 0;
+    }
 }
