aa85587e404558ae7bba57f9c14204cf231f0fd8
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-15 12:45:09 +0200

    Multiplying the numbers of the main diagonal

--- src/main/java/sk/kosickaakademia/spivak/matrix/Matrix.java
+++ src/main/java/sk/kosickaakademia/spivak/matrix/Matrix.java
@@ -1,6 +1,11 @@
 package sk.kosickaakademia.spivak.matrix;
 
 public class Matrix {
+
+    /**
+     * Output of the matrix to the console
+     * @param matrix
+     */
     public void printMatrix (int matrix[][]){
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
@@ -8,5 +13,22 @@ public class Matrix {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * Multiplying the numbers of the main diagonal
+     * @param matrix
+     * @return int
+     */
+    public static int mainDiagonal (int matrix[][]){
+        int result = 1;
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(i == j){
+                    result = result * matrix[i][j];
+                }
+            }
+        }
+        return result;
     }
 }
