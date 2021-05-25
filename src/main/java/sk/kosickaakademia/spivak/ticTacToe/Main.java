3e0bbfe10d313fc5af2bdc51d23e9e90e509f5f4
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-25 21:52:32 +0200

    add method play version 1.0

--- src/main/java/sk/kosickaakademia/spivak/ticTacToe/Main.java
+++ src/main/java/sk/kosickaakademia/spivak/ticTacToe/Main.java
@@ -2,7 +2,8 @@ package sk.kosickaakademia.spivak.ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.play();
     }
 
 
--- src/main/java/sk/kosickaakademia/spivak/ticTacToe/TicTacToe.java
+++ src/main/java/sk/kosickaakademia/spivak/ticTacToe/TicTacToe.java
@@ -1,28 +1,33 @@
 package sk.kosickaakademia.spivak.ticTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    private int[][] pole;
+    private int player=1;
+    private int count=0;
 
     /**
      * Determines whether one of the players on the 5-on-5 field has won
-     * @param pole
+     * @param field
      * @return -1   Array not of size 5 by 5
      *          0   No one won
      *          1   The number one player won
      *          2   The number two player won
      */
-    public int isWinner5on5(int[][] pole){
-        if(pole.length != 5 || pole[0].length != 5){
+    private int isWinner5on5(int[][] field){
+        if(field.length != 5 || field[0].length != 5){
             return -1;
         }
         for(int i = 0; i < 5; i++){
-            if(pole[i][0]*pole[i][1]*pole[i][2]*pole[i][3]*pole[i][4]==1 || pole[0][i]*pole[1][i]*pole[2][i]*pole[3][i]*pole[4][i]==1)
+            if(field[i][0]*field[i][1]*field[i][2]*field[i][3]*field[i][4]==1 || field[0][i]*field[1][i]*field[2][i]*field[3][i]*field[4][i]==1)
                 return 1;
-            if(pole[i][0]*pole[i][1]*pole[i][2]*pole[i][3]*pole[i][4]==32 || pole[0][i]*pole[1][i]*pole[2][i]*pole[3][i]*pole[4][i]==8)
+            if(field[i][0]*field[i][1]*field[i][2]*field[i][3]*field[i][4]==32 || field[0][i]*field[1][i]*field[2][i]*field[3][i]*field[4][i]==8)
                 return 2;
         }
-        if(pole[0][0]*pole[1][1]*pole[2][2]*pole[3][3]*pole[4][4]==1 || pole[0][4]*pole[1][3]*pole[2][2]*pole[3][1]*pole[4][0]==1)
+        if(field[0][0]*field[1][1]*field[2][2]*field[3][3]*field[4][4]==1 || field[0][4]*field[1][3]*field[2][2]*field[3][1]*field[4][0]==1)
             return 1;
-        if(pole[0][0]*pole[1][1]*pole[2][2]*pole[3][3]*pole[4][4]==32 || pole[0][4]*pole[1][3]*pole[2][2]*pole[3][1]*pole[4][0]==32)
+        if(field[0][0]*field[1][1]*field[2][2]*field[3][3]*field[4][4]==32 || field[0][4]*field[1][3]*field[2][2]*field[3][1]*field[4][0]==32)
             return 2;
         return 0;
     }
@@ -35,7 +40,7 @@ public class TicTacToe {
      *          1   The number one player won
      *          2   The number two player won
      */
-    public int isWinnerNxN(int[][] field){
+    private int isWinnerNxN(int[][] field){
         if(field.length != field[0].length || field.length < 5){
             return -1;
         }
@@ -62,34 +67,44 @@ public class TicTacToe {
 
     /**
      * Filling an array with zeros
-     * @param array
+     * @param field
      */
-    public void initArray(int[][] array) {
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array[0].length; j++){
-                array[i][j] = 0;
+    private void initArray(int[][] field) {
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j < field[0].length; j++){
+                field[i][j] = 0;
             }
         }
     }
 
     /**
      * Output an array to the console
-     * @param array
+     * @param field
      */
-    public void printGame(int[][] array) {
-        for(int i=0;i< array.length;i++){
+    private void printGame(int[][] field) {
+        for(int i=0;i< field.length;i++){
             System.out.print("-" + "\t");
         }
         System.out.println();
-        for(int i=0;i< array.length;i++){
-            for(int j=0;j< array[0].length;j++){
-                System.out.print(array[i][j] + "\t");
+        for(int i=0;i< field.length;i++){
+            for(int j=0;j< field[0].length;j++){
+                System.out.print(field[i][j] + "\t");
             }
             System.out.println();
         }
-        for(int i=0;i< array.length;i++){
+        for(int i=0;i< field.length;i++){
             System.out.print("-" + "\t");
         }
         System.out.println();
+    }
+
+    public void play(){
+        System.out.println("This is a 5-in-a-row tic-tac-toe game!");
+        System.out.println("Enter the size of the field:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] field = new int[n][n];
+        initArray(field);
+        printGame(field);
     }
 }
