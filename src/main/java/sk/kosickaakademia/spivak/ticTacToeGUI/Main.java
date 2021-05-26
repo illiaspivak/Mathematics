066a24e66f9c70bdc0fe7ab0bdfcf9f35d8e51e6
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-05-26 10:47:13 +0200

    add game Tic Tac Toe 3x3 GUI (empty field)

---
+++ src/main/java/sk/kosickaakademia/spivak/ticTacToeGUI/Main.java
@@ -0,0 +1,45 @@
+package sk.kosickaakademia.spivak.ticTacToeGUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+
+
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(600, 600);
+
+        return root;
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
